518ca57ffcb50b55278a89a4cc9b0588ed9ea560
Filipe Falco <falcoefilipe@hotmail.com>
2021-06-22 15:26:05 -0400

    [UPDATE] Implemented the register of user

--- app/src/main/java/com/filipefalco/notesapp/Registro.java
+++ app/src/main/java/com/filipefalco/notesapp/Registro.java
@@ -1,5 +1,6 @@
 package com.filipefalco.notesapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,11 +11,19 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Registro extends AppCompatActivity {
 
     private EditText mRegistroEmail, mRegistroPassword;
     private RelativeLayout mRegistro;
     private TextView mVoltarLogin;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,9 @@ public class Registro extends AppCompatActivity {
         mRegistroPassword = findViewById(R.id.registroPassword);
         mRegistro = findViewById(R.id.registrar);
         mVoltarLogin = findViewById(R.id.voltarLogin);
+
+        // pegar instancia firebase
+        firebaseAuth = FirebaseAuth.getInstance();
 
         mVoltarLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,8 +60,38 @@ public class Registro extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Senha tem que ter mais de 7 caracteres", Toast.LENGTH_SHORT).show();
                 } else {
                      // registrar usuario no firebase
+                    firebaseAuth.createUserWithEmailAndPassword(mail, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "Registrdo com sucesso", Toast.LENGTH_SHORT).show();
+                                sendEmailVerification();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Falha ao registrar", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
                 }
             }
         });
+    }
+
+    // enviar verificação de email
+    private void sendEmailVerification() {
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser != null) {
+            firebaseUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    Toast.makeText(getApplicationContext(), "E-mail de verificação enviado, verifique o e-mail e faça o login", Toast.LENGTH_SHORT).show();
+                    firebaseAuth.signOut();
+                    finish();
+                    startActivity(new Intent(Registro.this, MainActivity.class));
+                }
+            });
+        } else {
+            Toast.makeText(getApplicationContext(), "Falha ao enviar o e-mail de verificação", Toast.LENGTH_SHORT).show();
+        }
     }
 }
