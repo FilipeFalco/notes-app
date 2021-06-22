1aa13d856061531459da80b92942eb7ca28d7718
Filipe Falco <falcoefilipe@hotmail.com>
2021-06-22 17:41:13 -0400

    [UPDATE] Implemented the forget password logic

--- app/src/main/java/com/filipefalco/notesapp/EsqueciSenha.java
+++ app/src/main/java/com/filipefalco/notesapp/EsqueciSenha.java
@@ -1,5 +1,6 @@
 package com.filipefalco.notesapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,11 +11,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class EsqueciSenha extends AppCompatActivity {
 
     private EditText mEsqueceuSenha;
     private Button mRecuperarSenhaButton;
     private TextView mVoltarLogin;
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,8 @@ public class EsqueciSenha extends AppCompatActivity {
         mEsqueceuSenha = findViewById(R.id.esqueceuSenha);
         mRecuperarSenhaButton = findViewById(R.id.recuperarSenha);
         mVoltarLogin = findViewById(R.id.voltarLogin);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         mVoltarLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +52,18 @@ public class EsqueciSenha extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Entre com seu e-mail primeiramente", Toast.LENGTH_SHORT).show();
                 } else {
                     // temos que enviar email para recuperação de senha
+                    firebaseAuth.sendPasswordResetEmail(mail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "E-mail enviado! Verifique o e-mail enviado para recuperar a senha", Toast.LENGTH_LONG).show();
+                                finish();
+                                startActivity(new Intent(EsqueciSenha.this, MainActivity.class));
+                            } else {
+                                Toast.makeText(getApplicationContext(), "E-mail esta incorreto ou a conta não existe", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
                 }
             }
         });
