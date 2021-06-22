a290c7c5124080aab4530a396ae3dc59d6d5b9a1
Filipe Falco <falcoefilipe@hotmail.com>
2021-06-22 17:41:43 -0400

    [NEW] Created Notes page

---
+++ app/src/main/java/com/filipefalco/notesapp/NotasActivity.java
@@ -0,0 +1,14 @@
+package com.filipefalco.notesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notas);
+    }
+}
