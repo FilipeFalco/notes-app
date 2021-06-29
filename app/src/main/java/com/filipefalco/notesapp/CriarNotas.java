9d89e81a2e3b807a08a1bc896b6a1f350608d228
Lblaster <lucas.avila.mendes.lam@gmail.com>
2021-06-29 14:46:32 -0400

    criação das notas e visualização das notas

--- app/src/main/java/com/filipefalco/notesapp/CriarNotas.java
+++ app/src/main/java/com/filipefalco/notesapp/CriarNotas.java
@@ -1,14 +1,93 @@
 package com.filipefalco.notesapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CriarNotas extends AppCompatActivity {
+
+    EditText criarTituloDaNota, criarConteudoDaNota;
+    FloatingActionButton salvarNota;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criar_notas);
+
+        salvarNota = findViewById(R.id.salvarNotaFab);
+        criarTituloDaNota = findViewById(R.id.criarTituloDaNota);
+        criarConteudoDaNota = findViewById(R.id.criarConteudoDaNota);
+
+        Toolbar toolbar = findViewById(R.id.toolBarCriarNotas);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        salvarNota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = criarTituloDaNota.getText().toString();
+                String content = criarConteudoDaNota.getText().toString();
+                if (title.isEmpty() || content.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Ambos os campos são necessários", Toast.LENGTH_SHORT).show();
+                }else{
+                    DocumentReference documentReference = firebaseFirestore.collection("notes").document(firebaseUser.getUid()).collection("myNotes").document();
+                    Map<String, Object> note = new HashMap<>();
+                    note.put("title", title);
+                    note.put("content", content);
+
+                    documentReference.set(note).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(getApplicationContext(), "Nota criada com sucesso", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(CriarNotas.this, NotasActivity.class));
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), "Erro ao criar a nota", Toast.LENGTH_SHORT).show();
+                            //startActivity(new Intent(CriarNotas.this, NotasActivity.class));
+
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==android.R.id.home){
+            onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/filipefalco/notesapp/NotasActivity.java
+++ app/src/main/java/com/filipefalco/notesapp/NotasActivity.java
@@ -5,17 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+
+//import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class NotasActivity extends AppCompatActivity {
 
     FloatingActionButton mCriarNotaFab;
     private FirebaseAuth firebaseAuth;
+
+    RecyclerView recyclerView;
+    StaggeredGridLayoutManager staggeredGridLayoutManager;
+    FirebaseUser firebaseUser;
+    FirebaseFirestore firebaseFirestore;
+    FirestoreRecyclerAdapter<FirebaseModel, NoteViewHolder> noteAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +47,8 @@ public class NotasActivity extends AppCompatActivity {
 
         mCriarNotaFab = findViewById(R.id.criarNotaFab);
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseFirestore = FirebaseFirestore.getInstance();
 
         getSupportActionBar().setTitle("Todas as Notas");
 
@@ -33,7 +58,52 @@ public class NotasActivity extends AppCompatActivity {
                 startActivity(new Intent(NotasActivity.this, CriarNotas.class));
             }
         });
+
+        Query query = firebaseFirestore.collection("notes").document(firebaseUser.getUid()).collection("myNotes").orderBy("title", Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<FirebaseModel> allUserNotes = new FirestoreRecyclerOptions.Builder<FirebaseModel>().setQuery(query, FirebaseModel.class).build();
+
+        noteAdapter = new FirestoreRecyclerAdapter<FirebaseModel, NoteViewHolder>(allUserNotes) {
+            @Override
+            protected void onBindViewHolder(@NonNull NoteViewHolder holder, int position, @NonNull FirebaseModel model) {
+
+                holder.notetitle.setText(model.getTitle());
+                holder.notecontent.setText(model.getContent());
+
+            }
+
+            @NonNull
+            @Override
+            public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+               View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.notas_layout, viewGroup, false);
+               return new NoteViewHolder(view);
+            }
+        };
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
+        recyclerView.setAdapter(noteAdapter);
+
     }
+
+
+    public class NoteViewHolder extends RecyclerView.ViewHolder{
+        private TextView notetitle;
+        private TextView notecontent;
+        LinearLayout note;
+
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            notetitle = itemView.findViewById(R.id.notaTitulo);
+            notecontent = itemView.findViewById(R.id.conteudoNota);
+            note = itemView.findViewById(R.id.nota);
+
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,5 +121,17 @@ public class NotasActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
+    protected void onStop() {
+        super.onStop();
+        if (noteAdapter != null){
+            noteAdapter.startListening();
+        }
     }
 }
